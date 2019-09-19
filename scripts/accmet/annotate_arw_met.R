4928b009b9810b4537d444d62517996873303327
ashrosewood <ashleyrosewoodfin@gmail.com>
2019-09-19 10:06:05 -0700

    initial commit

---
+++ scripts/accmet/annotate_arw_met.R
@@ -0,0 +1,110 @@
+library(data.table)
+library(purrr)
+library(furrr)
+
+
+# quantify methylation rates over genomic annotations
+# input is raw methylation data (1 file per cell)
+# output is quantified methylation data (1 file per annotation)
+
+io <- list()
+
+io$anno_dir <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/features/filt"
+io$raw_files <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/met/raw"
+io$meta_data <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/sample_metadata.tsv"
+io$out_dir <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/met/parsed"
+
+dir.create(io$out_dir, recursive = TRUE)
+
+opts <- list()
+opts$anno_regex <- "" # use to select specific annotations
+opts$parallel <- FALSE
+opts$cores <- 2
+opts$gzip <- TRUE
+opts$extend_anno_len <- FALSE
+opts$anno_len <- 500
+
+
+### functions ###
+fread_gz = function(filename, ...){
+  f <- file(filename)
+  type <- summary(f)$class
+  close.connection(f)
+  if (type == "gzfile") return(fread(cmd = paste("zcat", filename), ...))
+  fread(filename, ...)
+}
+
+
+### load metadata and select cells/files ###
+
+meta <- fread(io$meta_data)
+
+if (grepl("met", io$raw_files)) {
+   meta <- meta[pass_metQC == TRUE]
+   cells <- meta[, sample]
+   files <- meta[, paste0(io$raw_files, "/", id_met, ".tsv.gz")]
+} else {
+  meta <- meta[pass_accQC == TRUE]
+  cells <- meta[, sample]
+  files <- meta[, paste0(io$raw_files, "/", id_acc, ".tsv.gz")]
+}
+
+cells <- cells[file.exists(files)]
+files <- files[file.exists(files)]
+
+
+### load annotations ###
+
+anno <- dir(io$anno_dir, pattern = ".bed", full = TRUE) %>%
+  .[grep(opts$anno_regex, .)]%>%
+#    map2(., sub(".bed", "", basename(.)), ~fread(.x, colClasses = list("factor" = 1L)) %>% 
+#           setnames(c("chr", "start", "end", "strand", "id", "anno")) %>% 
+#           .[, anno := paste0(anno, "_", .y)]) %>%
+    map(fread, colClasses = list("character" = 1)) %>%
+  rbindlist() %>% setnames(c("chr", "start", "end", "strand", "id", "anno")) %>%
+  setkey(chr, start, end)
+
+if (opts$extend_anno_len) {
+  anno <- anno[, mid := start + (end-start)/2] 
+  anno[end-start < opts$anno_len, c("start", "end") := .(as.integer(round(mid - opts$anno_len/2), round(mid + opts$anno_len/2)))]
+  setkey(anno, chr, start, end)
+}
+
+
+### perform overlap and quantification ###
+
+
+if (opts$parallel) {
+  plan(multiprocess, workers = opts$cores)
+} else {
+  plan(sequential)
+}
+
+acc_dt <- future_map2(files, cells, ~{
+#acc_dt <- map2(files, cells, ~{
+  if (!file.exists(.x)) return(NULL)
+  fread(.x, select=c(1:2,5), colClasses = list("factor" = 1L)) %>%
+  #fread_gz(.x, select = c(1:2, 5), colClasses = list("factor" = 1L)) %>%
+   # .[, .(chr = gsub("chr", "", chr),
+    .[, .(chr = chr,
+          start = pos,
+          end = pos,
+          rate)] %>%
+    setkey(chr, start, end) %>%
+    foverlaps(anno, nomatch = 0L) %>% 
+    .[, .(rate = round(100 * mean(rate)), .N, sample = .y), .(id, anno)]
+}) %>%
+  purrr::compact() %>%
+  rbindlist()
+
+setkey(acc_dt, anno)
+acc_dt <- split(acc_dt, by = "anno")
+
+# save one file per anno
+file_names <- paste0(io$out_dir, "/", names(acc_dt), ".tsv")
+if (opts$extend_anno_len) file_names <- sub(".tsv", "_", opts$anno_len, "bp.tsv")
+
+# future_map2(acc_dt, file_names, fwrite, sep = "\t")
+map2(acc_dt, file_names, fwrite, sep = "\t")
+
+if (opts$gzip) walk(file_names, ~system(paste("gzip -f", .)))
---
+++ scripts/accrna/correlations_acc.R
@@ -0,0 +1,160 @@
+library(SingleCellExperiment)
+library(scater)
+library(data.table)
+library(purrr)
+library(weights)
+library(ggplot2)
+library(cowplot)
+library(ggrepel)
+
+
+io <- list()
+io$meta_data <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/sample_metadata.tsv"
+io$met_dir <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/acc/parsed"
+io$rna_sce <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/rna/parsed_hg19/SingleCellExperiment.rds"
+io$anno_dir <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/features/filt/"
+io$gene_file <- "Box/My_NMT-seq_work/Stephen/code/MCF7_matched-selected/features/genes_hg19/gene_metadata.tsv.gz"
+
+opts <- list()
+opts$anno_regex <- ""
+opts$gene_overlap_dist <- 1e4 # overlap annoations with genes within xx bp
+opts$min_weight_met <- 1
+opts$min_cells_met <- 20 # loci must have observations in this many cells
+opts$min_cells_rna <- 5 # genes must have exp>0 in this many cells
+
+opts$filt_rna_var <- 0.5 # select the top xx fraction by variance
+opts$filt_met_var <- 0.5 # select the top xx fraction by variance
+
+opts$p_cutoff <- 0.1
+
+
+### functions ###
+fread_gz <- function(path, ...){fread(cmd = paste("zcat", path), ...)}
+
+fread_gz <- function(path, ...){fread(x, ...)}
+
+### load metadata and select cells ####
+
+meta <- fread(io$meta_data) %>%
+  .[pass_accQC == TRUE & pass_metQC == TRUE & pass_rnaQC == TRUE]
+
+### load rna and format as data.table ###
+
+rna <- readRDS(io$rna_sce) %>%
+  .[, meta[, id_rna]] %>%
+  exprs() %>%
+  as.data.frame() %>%
+  setDT(keep.rownames = "ens_id") %>%
+  melt(id.vars = "ens_id", value.name = "exp", variable.name = "id_rna") %>%
+  merge(meta[, .(id_rna, sample)], by = "id_rna")
+
+
+### filter rna ###
+
+rna <- rna[, keep := sum(exp>0) > opts$min_cells_rna, ens_id] %>%
+  .[keep == TRUE]
+
+filt_genes_var <- rna[, var(exp), ens_id] %>%
+  .[order(-rank(V1))] %>%
+  .[1: (.N * opts$filt_rna_var), ens_id]
+
+rna <- rna[ens_id %in% filt_genes_var]
+
+
+### load met data ###
+
+met <- dir(io$met_dir, pattern = ".tsv.gz$", full = TRUE) %>%
+  .[grep(opts$anno_regex, .)] %>%
+  # map(fread_gz) %>%
+  map(fread) %>%
+  rbindlist()
+
+
+
+### annotate with gene name and keep only loci with nearby genes ###
+
+genes <- fread(io$gene_file) %>%
+  .[, c("start", "end", "chr") := .(start - opts$gene_overlap_dist, 
+                                    end + opts$gene_overlap_dist,
+                                    gsub("chr", "", chr))] %>%
+  setkey(chr, start, end)
+
+anno <- dir(io$anno_dir, full = TRUE, pattern = ".bed$") %>%
+  .[grep(opts$anno_regex, .)] %>%
+  map(fread) %>%
+  rbindlist() %>% setnames(c("chr", "start", "end", "strand", "id", "anno")) %>%
+  .[, .(chr = gsub("chr", "", chr),
+        start = start,
+        end = end,
+        strand = strand,
+        id = id,
+        anno = anno
+  )] %>% .[!chr %in% c("MT", "Y")] %>%
+  .[, gene_id := grepl("ENSG", id)] %>%
+  split(by = "gene_id", keep.by = FALSE)
+
+anno[["TRUE"]] <- anno[["TRUE"]] %>%
+  .[, ens_id := id] %>%
+  merge(genes[, .(ens_id, gene)], by = "ens_id")
+
+anno[["FALSE"]] <- setkey(anno[["FALSE"]], chr, start, end) %>%
+  foverlaps(genes, nomatch = 0L)
+
+anno <- map(anno, ~.[, .(id, anno, gene, ens_id)]) %>%
+  rbindlist()
+
+met <- merge(met, anno, by = c("anno", "id"), allow.cartesian = TRUE) # note some loci have >1 gene -> cartesian join
+
+### join met with rna ###
+
+metrna <- merge(met, rna, by = c("ens_id", "sample"))
+
+
+
+### filter met data ###
+
+metrna <- metrna[N >= opts$min_weight_met] %>%
+  .[, keep := .N > opts$min_cells_met, .(id, anno, gene, ens_id)] %>%
+  .[keep == TRUE]
+
+filt_met_var <- metrna[, var(rate), .(id, anno, gene, ens_id)] %>%
+  unique(by = c("id", "anno", "V1")) %>% # need to filter out multiple instances due to some loci having >1 gene
+  split(by = "anno") %>%
+  map(~.[order(-rank(V1))][1: (.N * opts$filt_met_var), .(id, anno)]) %>%
+  rbindlist() %>%
+  setkey(id, anno)
+
+metrna <- metrna[filt_met_var, on = c("id", "anno")]
+
+### compute correlation coeff ###
+
+compute_cor <- function(exp, rate, N){
+  wtd.cor(exp, rate, N) %>%
+    as.list() %>%
+    set_names(c("r", "std_err", "t", "p"))
+}
+
+cors <- metrna[, compute_cor(exp, rate, N), .(anno, id, gene, ens_id)] %>%
+  .[, padj := p.adjust(p, method = "fdr"), .(anno)] %>%
+  .[, logpadj := -log10(padj)] %>%
+  .[, sig := padj < opts$p_cutoff]
+
+# labs <- paste0(c("q < ", "q >= "), opts$p_cutoff)
+labs <- c("NS", "Significant")
+
+p <- ggplot(cors, aes(r, logpadj, colour = sig, label = gene)) +
+  geom_point() +
+  facet_wrap(~anno) +
+  scale_colour_manual(values = c("grey", "navy"), labels = labs, name = NULL) +
+  geom_hline(yintercept = -log10(opts$p_cutoff), colour = "blue", linetype = "dashed") +
+  geom_vline(aes(xintercept = mean(r)), colour = "blue") +
+  #geom_text_repel(data = cors[sig == TRUE]) +
+  labs(x = c("Weighted Pearson R"), y = "-log10 q-value") +
+  guides(label = FALSE) +
+  theme_bw()
+p
+
+
+save_plot("Box/My_NMT-seq_work/Stephen/my_plots/corr/acc_rna_correlations.pdf", p, base_width = 12, base_height = 12)
+
+
